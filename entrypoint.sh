100d28ea5200a2877108bf891fe9db3ab413d6ba
hafiz-syed-burhan <151712730+hafiz-syed-burhan@users.noreply.github.com>
2026-03-12 21:43:26 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ echo "burhan" | vncpasswd -f > ~/.vnc/passwd
 chmod 600 ~/.vnc/passwd
 
 # 4. VNC Server ko Start karna (Pehle server chalna chahiye)
-tightvncserver :1 -geometry 1800x730 -depth 24
+tightvncserver :1 -geometry 1700x730 -depth 24
 
 # 5. NoVNC Proxy start karna (Background mein &)
 /usr/share/novnc/utils/launch.sh --vnc localhost:5901 --listen 6080 &
